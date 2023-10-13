cbf055bd406405bd6aa8e6eb3ee9a8f64aab6860
zuohuaihan <zuohh@nancal.com>
2023-10-13 20:03:33 +0800

    change:缓存

--- src/views/HistoricalVersion/hooks/index.ts
+++ src/views/HistoricalVersion/hooks/index.ts
@@ -41,7 +41,7 @@ export const useTable = () => {
     const data = <any>await requestCommonGetHistoryList({
       className: 'CompanyItem',
       thisObj: {
-        objId: objId,
+        objId: '1704055851523801088',
       },
     });
     tableData.value = data.data;
