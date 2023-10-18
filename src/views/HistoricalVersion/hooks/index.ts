d70ba009abfac8c026e5fa1f160be1eed04288b4
zuohuaihan <zuohh@nancal.com>
2023-10-18 14:39:31 +0800

    change:解决数据加载时机,点击比较，并且只有第一次才会加载

--- src/views/HistoricalVersion/hooks/index.ts
+++ src/views/HistoricalVersion/hooks/index.ts
@@ -35,11 +35,12 @@ export const useTable = () => {
   ];
   const tableData = ref<DataItem[]>([]);
   const getList = async () => {
-    // const objId = window.$wujie?.props.params.record.objId;
+    const objId = window.$wujie?.props.params.record.objId;
+    //'1704055851523801088'
     const data = await requestCommonGetHistoryList({
       className: 'CompanyItem',
       thisObj: {
-        objId: '1704055851523801088',
+        objId: objId,
       },
     });
     tableData.value = data.data;
